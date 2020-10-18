d384bd9b9b1c6120dba8c7802d019d4397c01048
letterpair <hellok-taka@docomo.ne.jp>
2020-10-18 20:01:58 +0900

    update segtree

--- ACL/segtree.cpp
+++ ACL/segtree.cpp
@@ -41,8 +41,8 @@ struct segtree {
   void update(int k) { d[k] = op(d[2*k], d[2*k+1]);}
 };
 // Rewrite the following!!
-int e() { return -1001001001;}
 int op(int a, int b) { return max(a, b);}
+int e() { return -1001001001;}
 //
 
 int main() {
