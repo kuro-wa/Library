ced4c777767cd2347b5aa080a00312a30834416a
letterpair <hellok-taka@docomo.ne.jp>
2020-10-19 20:02:39 +0900

    update mf_graph

--- ACL/mf_graph.cpp
+++ ACL/mf_graph.cpp
@@ -28,7 +28,7 @@ struct mf_graph {
       que.push(s);
       while (!que.empty()) {
         int v = que.front(); que.pop();
-        for (auto e : g[v]) {
+        for (auto& e : g[v]) {
           if (e.cap == 0 || level[e.to] >= 0) continue;
           level[e.to] = level[v]+1;
           if (e.cap ==  t) return;
