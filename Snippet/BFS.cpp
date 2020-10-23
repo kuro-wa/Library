e7b0dc9c01cb0d4980317d77b3b0add4dfc39782
letterpair <hellok-taka@docomo.ne.jp>
2020-10-23 18:13:18 +0900

    update BFS

--- Snippet/BFS.cpp
+++ Snippet/BFS.cpp
@@ -1,21 +1,7 @@
-/*input
-6 9
-3 4
-6 1
-2 4
-5 3
-4 6
-1 5
-6 2
-4 5
-5 6
-*/
-
 #include <bits/stdc++.h>
-#define rep(i, n) for (int i = 0; i < (n); ++i)
 using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
 using ll = long long;
-using P = pair<int, int>;
 
 const int INF = 1001001001;
 
@@ -27,9 +13,9 @@ int main() {
 
   vector<int> dist(n, INF);
   queue<int> q;
-  auto push = [&](int v, int x) {
+  auto push = [&](int v, int d) {
     if (dist[v] != INF) return;
-    dist[v] = x;
+    dist[v] = d;
     q.push(v);
   };
   int s = 0; // the start
