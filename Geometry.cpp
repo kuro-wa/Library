6a78b2520d6bca4465867298cf62a700efe4d05c
letterpair <hellok-taka@docomo.ne.jp>
2020-10-24 20:27:53 +0900

    add Line

--- Geometry.cpp
+++ Geometry.cpp
@@ -1,17 +1,35 @@
 const double eps = 1e-10;
 bool equal(double a, double b) { return abs(a-b) < eps;}
 
+// 2D vector
 struct V {
   double x, y;
   V(double x=0, double y=0) : x(x), y(y) {}
   V operator+(const V& p) const { return V(x+p.x, y+p.y);}
   V operator-(const V& p) const { return V(x-p.x, y-p.y);}
   V operator*(double a) const { return V(x*a, y*a);}
+  friend V operator*(double a, const V& p) { return p*a;}
+  double dot(const V& p) { return x*p.x+y*p.y;}
+  double cross(const V& p) {return x*p.y-y*p.x;}
   double sqnorm() { return x*x+y*y;}
   double norm() { return sqrt(x*x+y*y);}
   V rot90() { return V(-y, x);}
 };
 
+// line segment
+struct Line {
+  V s, t;
+  Line(V s=V(), V t=V()) : s(s), t(t) {}
+  bool on(const V& p) {
+    return equal((s-p).cross(t-p), 0) && (s-p).dot(t-p) < eps;
+  }
+  V intersection(const Line& l) {
+    return s+(t-s)*((l.t-l.s).cross(l.s-s)/(l.t-l.s).cross(t-s));
+  }
+  bool parallel(const Line& l) { return equal((t-s).cross(l.t-l.s), 0);}
+};
+
+// circle
 struct Circle {
   V o;
   double r;
@@ -35,3 +53,6 @@ struct Circle {
     return (p-o).norm() < r+eps;
   }
 };
+
+ostream& operator<<(ostream& os, const V& p) { return os<<'('<<p.x<<','<<p.y<<')';}
+//
