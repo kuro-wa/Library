246d2b1ee50fd18f3512bf0528c7daf1131f7bfc
letterpair <hellok-taka@docomo.ne.jp>
2020-11-16 11:23:45 +0900

    update e

--- Snippet/RMQ.cpp
+++ Snippet/RMQ.cpp
@@ -1,26 +1,26 @@
 // 1. Segment Tree
 // 1-1. Range Min Query
 using S = int;
-const S IE = 1001001001
+const S IE = 1001001001;
 S op(S a, S b) { return min(a, b);}
 S e() { return IE;}
   vector<S> vec(n, S(0));
-  segtree<S, op, e> seg(vec);
+  segtree<S, op, e> d(vec);
 //
 // 1-2. Range Max Query
 using S = int;
-const S IE = -1001001001
+const S IE = -1001001001;
 S op(S a, S b) { return max(a, b);}
 S e() { return IE;}
   vector<S> vec(n, S(0));
-  segtree<S, op, e> seg(vec);
+  segtree<S, op, e> d(vec);
 //
 
 // 2. Lazy SegTree
 // 2-1. Range Add Query and Range Min Query
 using S = int;
 using F = int;
-const S IE = 1001001001
+const S IE = 1001001001;
 S op(S a, S b) { return min(a, b);}
 S e() { return IE;}
 S mapping(F f, S s) { return f+s;}
@@ -28,12 +28,12 @@ F composition(F f, F g) { return f+g;}
 F id() { return F(0);}
 using lseg = lazy_segtree<S, op, e, F, mapping, composition, id>;
   vector<S> vec(n, S(0));
-  lseg seg(vec);
+  lseg d(vec);
 //
 // 2-2. Range Add Query and Range Max Query
 using S = int;
 using F = int;
-const S IE = -1001001001
+const S IE = -1001001001;
 S op(S a, S b) { return max(a, b);}
 S e() { return IE;}
 S mapping(F f, S s) { return f+s;}
@@ -41,5 +41,5 @@ F composition(F f, F g) { return f+g;}
 F id() { return F(0);}
 using lseg = lazy_segtree<S, op, e, F, mapping, composition, id>;
   vector<S> vec(n, S(0));
-  lseg seg(vec);
+  lseg d(vec);
 //
