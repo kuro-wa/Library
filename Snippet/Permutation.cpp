6c5988fdd5d0c9f8f83ab8fb264f1920c7894ea5
letterpair <hellok-taka@docomo.ne.jp>
2020-11-27 14:32:11 +0900

    first commit

---
+++ Snippet/Permutation.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> p(n);
+  iota(p.begin(), p.end(), 0);
+  do {
+    // Do something
+  } while (next_permutation(p.begin(), p.end()));
+  return 0;
+}
