9407bb80aa65ac1029da93e06cd762e343e4a720
letterpair <hellok-taka@docomo.ne.jp>
2020-11-29 14:44:29 +0900

    add primes

--- Sieve.cpp
+++ Sieve.cpp
@@ -2,42 +2,42 @@
 // https://youtu.be/UTVg7wzMWQc?t=2774
 struct Sieve {
  public:
-  // primes[i]: i-th prime num (0-indexed)
-  vector<int> primes;
-  Sieve(int n=1):n(n), f(n+1) {
+  Sieve(int n=1) : n(n), f(n+1) {
     f[0] = f[1] = -1;
     for (long long i = 2; i <= n; ++i) {
       if (f[i]) continue;
-      primes.emplace_back(i);
+      ps.emplace_back(i);
       f[i] = i;
       for (long long j = i*i; j <= n; j += i) {
         if (!f[j]) f[j] = i;
       }
     }
   }
-  // Returns whether x (<= n) is a prime num
-  bool is_prime(int x) { return f[x] == x;}
-  // for long long x (<= n*n)
+  vector<int> primes() { return ps;}
+  bool is_prime(int x) {
+    assert(0 <= x && x <= n);
+    return f[x] == x;
+  }
   bool is_prime(long long x) {
-    for (int p : primes) {
+    assert(0 <= x && x <= (long long)n*n);
+    for (int p : ps) {
       if ((long long)p*p > x) break;
       if (x%p == 0) return false;
     }
     return x != 1;
   }
-  // Returns prime foctorization of x (<= n)
-  // res[i]: (i-th prime factor, the index)
   map<int, int> factor(int x) {
+    assert(0 <= x && x <= n);
     vector<int> fl = factor_list(x);
     if (fl.size() == 0) return {};
     map<int, int> res;
     for (int p : fl) ++res[p];
     return res;
   }
-  // for long long x (<= n*n)
   map<long long,int> factor(long long x) {
+    assert(0 <= x && x <= (long long)n*n);
     map<long long,int> res;
-    for (int p : primes) {
+    for (int p : ps) {
       int y = 0;
       while (x%p == 0) x /= p, ++y;
       if (y != 0) res[p] = y;
@@ -45,23 +45,21 @@ struct Sieve {
     if (x != 1) res[x] =1;
     return res;
   }
-  // Enumerates divisors of n
-  vector<long long> divisor(long long n) {
+  vector<long long> divisor(long long x) {
     vector<long long> res;
-    for (long long i = 1; i*i <= n; ++i) {
-      if (n%i == 0) {
+    for (long long i = 1; i*i <= x; ++i) {
+      if (x%i == 0) {
         res.emplace_back(i);
-        if(i*i != n) res.emplace_back(n/i);
+        if(i*i != x) res.emplace_back(x/i);
       }
     }
     sort(res.begin(), res.end());
     return res;
   }
  private:
-  int n; // the max num
-  vector<int> f; // f[i]: the min prime factor of i
-  // Returns prime foctors of x (<= n) in ascending order
-  // ex. 24: {2, 2, 2, 3}
+  int n;
+  vector<int> f;
+  vector<int> ps;
   vector<int> factor_list(int x) {
     vector<int> res;
     while (x != 1) {
