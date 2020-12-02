5ccb88b3010529d3f6e49b4e52260a4c4640b27f
letterpair <hellok-taka@docomo.ne.jp>
2020-12-02 22:51:53 +0900

    fix variable name

--- Snippet/RMQ.cpp
+++ Snippet/RMQ.cpp
@@ -4,16 +4,16 @@ using S = int;
 const S IE = 1001001001;
 S op(S a, S b) { return min(a, b);}
 S e() { return IE;}
-  vector<S> vec(n, S(0));
-  segtree<S, op, e> d(vec);
+  vector<S> v(n, S(0));
+  segtree<S, op, e> d(v);
 //
 // 1-2. Range Max Query
 using S = int;
 const S IE = -1001001001;
 S op(S a, S b) { return max(a, b);}
 S e() { return IE;}
-  vector<S> vec(n, S(0));
-  segtree<S, op, e> d(vec);
+  vector<S> v(n, S(0));
+  segtree<S, op, e> d(v);
 //
 
 // 2. Lazy SegTree
@@ -27,8 +27,8 @@ S mapping(F f, S s) { return f+s;}
 F composition(F f, F g) { return f+g;}
 F id() { return F(0);}
 using lseg = lazy_segtree<S, op, e, F, mapping, composition, id>;
-  vector<S> vec(n, S(0));
-  lseg d(vec);
+  vector<S> v(n, S(0));
+  lseg d(v);
 //
 // 2-2. Range Add Query and Range Max Query
 using S = int;
@@ -40,6 +40,6 @@ S mapping(F f, S s) { return f+s;}
 F composition(F f, F g) { return f+g;}
 F id() { return F(0);}
 using lseg = lazy_segtree<S, op, e, F, mapping, composition, id>;
-  vector<S> vec(n, S(0));
-  lseg d(vec);
+  vector<S> v(n, S(0));
+  lseg d(v);
 //
