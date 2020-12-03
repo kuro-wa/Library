10bedf06397d87de844f52afa92c610ac4f001f2
letterpair <hellok-taka@docomo.ne.jp>
2020-12-03 12:30:31 +0900

    update template

--- RSQ.cpp
+++ RSQ.cpp
@@ -1,14 +1,13 @@
 // RSQ (Range Sum Query) and RAQ (Range Add Query)
-template<typename T>
+template<typename T, typename F=T>
 struct RSQ {
-  using F = T;
  public:
   RSQ(int n=0) : RSQ(vector<T>(n, T(0))) {}
   RSQ(const vector<T>& v) {
     int n = int(v.size());
-    vector<S> w(n, S());
-    for (int i = 0; i < n; ++i) w[i].x = v[i];
-    d = lseg(w);
+    vector<S> w(n);
+    for (int i = 0; i < n; ++i) w[i] = S(v[i], 1);
+    d = lazy_segtree<S, op, e, F, mapping, composition, id>(w);
   }
   void set(int p, T x) { d.set(p, S(x, 1));}
   T get(int p) { return d.get(p).x;}
@@ -23,10 +22,9 @@ struct RSQ {
   };
   static S op(S a, S b) { return S(a.x+b.x, a.l+b.l);}
   static S e() { return S(T(0), 0);}
-  static S mapping(F f, S s) { return S(s.x+s.l*f, s.l);}
+  static S mapping(F f, S s) { return S(f*s.l+s.x, s.l);}
   static F composition(F f, F g) { return f+g;}
   static F id() { return F(0);}
-  using lseg = lazy_segtree<S, op, e, F, mapping, composition, id>;
-  lseg d;
+  lazy_segtree<S, op, e, F, mapping, composition, id> d;
 };
 //
